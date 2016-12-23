b30d49f7b9d4e0fa0c033d1b086b0b8d9c84f128
Joel Alves <joelalves@iMac-de-Joel.local>
2016-12-23 14:18:14 +0000

    update date model Movie add type

--- Movie+CoreDataProperties.swift
+++ Movie+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Movie+CoreDataProperties.swift
 //  Movies
 //
-//  Created by Joel Alves on 16/11/16.
+//  Created by Joel Alves on 23/12/16.
 //  Copyright © 2016 Joel Alves. All rights reserved.
 //
 
@@ -16,17 +16,18 @@ extension Movie {
         return NSFetchRequest<Movie>(entityName: "Movie");
     }
 
-    @NSManaged public var imdbRating: Double
     @NSManaged public var actors: String?
     @NSManaged public var director: String?
-    @NSManaged public var genre: String?
     @NSManaged public var duration: String?
-    @NSManaged public var released: NSDate?
+    @NSManaged public var genre: String?
     @NSManaged public var id: String?
-    @NSManaged public var poster: String?
+    @NSManaged public var imdbRating: Double
     @NSManaged public var overview: String?
-    @NSManaged public var year: String?
+    @NSManaged public var poster: String?
+    @NSManaged public var released: NSDate?
     @NSManaged public var title: String?
+    @NSManaged public var year: String?
+    @NSManaged public var type: String?
     @NSManaged public var user: NSSet?
 
 }
